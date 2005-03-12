a52674435a0b6f2d5bc86474824482e16ecc68dd
runico <runico@743d3761-b3d6-0310-bc02-fcc0d55c254a>
2005-03-12 01:01:58 +0000

    A dialog to wait for a process whose length is unknown

---
+++ trunk/src/waitingdialog.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Sergio Cambra García                            *
+ *   runico@users.berlios.de                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef WAITINGDIALOG_H
+#define WAITINGDIALOG_H
+
+#include <kprogress.h>
+
+class QTimer;
+
+/**
+@author Sergio Cambra
+ */
+
+class WaitingDialog : private KProgressDialog
+{
+	Q_OBJECT
+	public:
+		WaitingDialog( QWidget *parent=0, const char *name=0, const QString &caption=QString::null, const QString &text=QString::null, bool modal=false );
+		virtual void show();
+		void stop();
+
+	private slots:
+		void slotProgress();
+
+	private:
+		QTimer *m_timer;
+};
+
+#endif
